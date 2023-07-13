a969204c8c575ff04c121ea3eb0ec289704eca44
gsemir0418 <845217811@qq.com>
2023-07-13 15:51:00 +0800

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,12 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://gsemir0418.github.io/blog-docusaurus-preview/',
+  // 不用加二级域名
+  url: 'https://gsemir0418.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/',
-  baseUrl: process.env.BASE_URL,
+  baseUrl: '/blog-docusaurus-preview/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
